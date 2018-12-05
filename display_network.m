931ef86ea89b392f5c34e0d396048d5e08fa7e52
Fan Feng <fanfeng2015@gmail.com>
2018-12-05 16:17:23 -0500

    Added files via upload

--- display_network.m
+++ display_network.m
@@ -31,68 +31,67 @@ A = A - mean(A(:));
 if opt_graycolor, colormap(gray); end
 
 % compute rows, cols
-[L M]=size(A);
-sz=sqrt(L);
-buf=1;
+[L M] = size(A);
+sz = sqrt(L);
+buf = 1;
 if ~exist('cols', 'var')
     if floor(sqrt(M))^2 ~= M
-        n=ceil(sqrt(M));
-        while mod(M, n)~=0 && n<1.2*sqrt(M), n=n+1; end
-        m=ceil(M/n);
+        n = ceil(sqrt(M));
+        while mod(M, n) ~= 0 && n < 1.2*sqrt(M), n = n + 1; end
+        m = ceil(M/n);
     else
-        n=sqrt(M);
-        m=n;
+        n = sqrt(M);
+        m = n;
     end
 else
     n = cols;
     m = ceil(M/n);
 end
 
-array=-ones(buf+m*(sz+buf),buf+n*(sz+buf));
+array = -ones(buf+m*(sz+buf), buf+n*(sz+buf));
 
 if ~opt_graycolor
-    array = 0.1.* array;
+    array = 0.1 .* array;
 end
 
-
 if ~opt_colmajor
-    k=1;
-    for i=1:m
-        for j=1:n
-            if k>M, 
+    k = 1;
+    for i = 1 : m
+        for j = 1 : n
+            if k > M 
                 continue; 
             end
-            clim=max(abs(A(:,k)));
+            clim = max(abs(A(:, k)));
             if opt_normalize
-                array(buf+(i-1)*(sz+buf)+(1:sz),buf+(j-1)*(sz+buf)+(1:sz))=reshape(A(:,k),sz,sz)/clim;
+                array(buf+(i-1)*(sz+buf)+(1:sz), buf+(j-1)*(sz+buf)+(1:sz)) = reshape(A(:, k), sz, sz) / clim;
             else
-                array(buf+(i-1)*(sz+buf)+(1:sz),buf+(j-1)*(sz+buf)+(1:sz))=reshape(A(:,k),sz,sz)/max(abs(A(:)));
+                array(buf+(i-1)*(sz+buf)+(1:sz), buf+(j-1)*(sz+buf)+(1:sz)) = reshape(A(:, k), sz, sz) / max(abs(A(:)));
             end
-            k=k+1;
+            k = k + 1;
         end
     end
 else
-    k=1;
-    for j=1:n
-        for i=1:m
-            if k>M, 
+    k = 1;
+    for j = 1 : n
+        for i = 1 : m
+            if k > M
                 continue; 
             end
-            clim=max(abs(A(:,k)));
+            clim = max(abs(A(:, k)));
             if opt_normalize
-                array(buf+(i-1)*(sz+buf)+(1:sz),buf+(j-1)*(sz+buf)+(1:sz))=reshape(A(:,k),sz,sz)/clim;
+                array(buf+(i-1)*(sz+buf)+(1:sz), buf+(j-1)*(sz+buf)+(1:sz)) = reshape(A(:, k), sz,sz) / clim;
             else
-                array(buf+(i-1)*(sz+buf)+(1:sz),buf+(j-1)*(sz+buf)+(1:sz))=reshape(A(:,k),sz,sz);
+                array(buf+(i-1)*(sz+buf)+(1:sz), buf+(j-1)*(sz+buf)+(1:sz)) = reshape(A(:, k), sz, sz);
             end
-            k=k+1;
+            k = k + 1;
         end
     end
 end
 
 if opt_graycolor
-    h=imagesc(array,'EraseMode','none',[-1 1]);
+    h = imagesc(array, 'EraseMode', 'none', [-1 1]);
 else
-    h=imagesc(array,'EraseMode','none',[-1 1]);
+    h = imagesc(array, 'EraseMode', 'none', [-1 1]);
 end
 axis image off
 
